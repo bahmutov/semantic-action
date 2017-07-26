62f2847c79c8ac6c5a3f1972c0000ab211859bd6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-07-26 10:03:15 -0400

    use standard and prettier

--- src/pre.js
+++ src/pre.js
@@ -8,39 +8,66 @@ var getType = require('./lib/type')
 module.exports = function (config, cb) {
   var plugins = config.plugins
 
-  auto({
-    lastRelease: plugins.getLastRelease.bind(null, config),
-    commits: ['lastRelease', function (results, cb) {
-      getCommits(_.assign({
-        lastRelease: results.lastRelease
-      }, config),
-      cb)
-    }],
-    type: ['commits', 'lastRelease', function (results, cb) {
-      getType(_.assign({
-        commits: results.commits,
-        lastRelease: results.lastRelease
-      }, config),
-      cb)
-    }]
-  }, function (err, results) {
-    if (err) return cb(err)
-
-    var nextRelease = {
-      type: results.type,
-      version: results.type === 'initial'
-        ? '1.0.0'
-        : semver.inc(results.lastRelease.version, results.type)
-    }
-
-    console.log('verifyRelease')
-    plugins.verifyRelease(_.assign({
-      commits: results.commits,
-      lastRelease: results.lastRelease,
-      nextRelease: nextRelease
-    }, config), function (err) {
+  auto(
+    {
+      lastRelease: plugins.getLastRelease.bind(null, config),
+      commits: [
+        'lastRelease',
+        function (results, cb) {
+          getCommits(
+            _.assign(
+              {
+                lastRelease: results.lastRelease
+              },
+              config
+            ),
+            cb
+          )
+        }
+      ],
+      type: [
+        'commits',
+        'lastRelease',
+        function (results, cb) {
+          getType(
+            _.assign(
+              {
+                commits: results.commits,
+                lastRelease: results.lastRelease
+              },
+              config
+            ),
+            cb
+          )
+        }
+      ]
+    },
+    function (err, results) {
       if (err) return cb(err)
-      cb(null, nextRelease)
-    })
-  })
+
+      var nextRelease = {
+        type: results.type,
+        version:
+          results.type === 'initial'
+            ? '1.0.0'
+            : semver.inc(results.lastRelease.version, results.type)
+      }
+
+      console.log('verifyRelease')
+      plugins.verifyRelease(
+        _.assign(
+          {
+            commits: results.commits,
+            lastRelease: results.lastRelease,
+            nextRelease: nextRelease
+          },
+          config
+        ),
+        function (err) {
+          if (err) return cb(err)
+          cb(null, nextRelease)
+        }
+      )
+    }
+  )
 }
